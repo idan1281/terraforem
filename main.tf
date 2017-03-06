f91092da4456a2accd7cb6064c34165d2073992a
idan mishor <idan.mishor@sap.com>
2017-03-06 14:12:51 +0200

    adding db server to TF

--- main.tf
+++ main.tf
@@ -14,20 +14,100 @@ variable "ssh_key_path" { default = "/Users/c5240533/.ssh/id_rsa" } # On macOS, 
 provider "openstack" {
 }
 
-# create an FIP
+# create an FIP for app
 resource "openstack_networking_floatingip_v2" "app_ip"
 {
         pool = "FloatingIP-internal-monsoon3"
 }
 
-resource "openstack_compute_instance_v2" "app_instance" 
+# create an FIP for DB
+resource "openstack_networking_floatingip_v2" "db_ip"
 {
-  name = "app"
+        pool = "FloatingIP-internal-monsoon3"
+}
+
+## installing DB server
+resource "openstack_compute_instance_v2" "db_instance"
+{
+  name = "idan-s4h-db"
   region = "${var.region}"
   image_name = "${var.image}"
-  flavor_id = "${var.flavor}"
+  flavor_id = "90"
+  key_pair = "${var.key_pair}"
+  security_groups = [
+    "default"]
+
+  # Connection details
+  connection
+  {
+    user = "${ var.ssh_user_name }"
+    private_key = "${file("${var.ssh_key_path}")}"
+    agent = false
+  }
+
+  # Create an internal ip and attach floating ip to it
+  network
+  {
+    uuid = "431361d3-e329-4f1b-9135-2819a3e9c6cd"
+    name = "Private-corp-sap-shared-01"
+    floating_ip = "${openstack_networking_floatingip_v2.db_ip.address}"
+    access_network = true
+    # Whether to use this network to access the instance or provision
+  }
+}
+  #Post Install Script after instance creation
+  resource "null_resource" "db" {
+    triggers {
+      db_instance_id = "${ openstack_compute_instance_v2.db_instance.id }"
+    }
+
+    # Connection details to do provision
+    connection {
+      host = "${ openstack_networking_floatingip_v2.db_ip.address  }"
+      user = "${ var.ssh_user_name }"
+      private_key = "${file("${var.ssh_key_path}")}"
+      agent = false
+    }
+
+    # Calling lyra_install script which takes care of lyra client installation locally.
+    provisioner "local-exec" "call_lyra_script" {
+      command = "scripts/lyra_install.sh ${openstack_compute_instance_v2.db_instance.id} ${ var.guest_os }"
+    }
+
+    # Script to run on target instance to clean up previous arc installation, useful for subsequent provisions.
+    provisioner "remote-exec" {
+      inline = [
+        "sudo systemctl stop arc.service",
+        "sudo rm -rf /opt/arc"
+      ]
+    }
+
+    # Copy the three lines arc install script to target instance and run it from there
+    provisioner "remote-exec" {
+      script = "tmp/INS_${openstack_compute_instance_v2.db_instance.id}.sh"
+    }
+
+    # Execute and watch create_file_2 automation
+    provisioner "local-exec" "call_automation_script" {
+      command = "scripts/automation_create_db.sh ${openstack_compute_instance_v2.db_instance.id}"
+    }
+  }
+
+
+
+  ####################
+
+
+
+resource "openstack_compute_instance_v2" "app_instance"
+{
+  name = "idan-s4happ"
+  region = "${var.region}"
+  image_name = "${var.image}"
+  flavor_id = "40"
   key_pair = "${var.key_pair}"
   security_groups = ["default"]
+
 
   # Connection details
   connection 
@@ -85,14 +165,14 @@ resource "null_resource" "app" {
   }
 
 # export the server name to a file
-  provisioner "local-exec" {
-        command = "echo ${openstack_compute_instance_v2.app_instance.name} >> scripts/srv_name"
-    }
+#  provisioner "local-exec" {
+#        command = "echo ${openstack_compute_instance_v2.app_instance.name} >> scripts/srv_name"
+#    }
 
 # run a script to read the server name from the file created in previous step and generate attribute file in json format
-  provisioner  "local-exec" "create_json_script" {
-    command = "scripts/create_json_script.sh"
-}
+#  provisioner  "local-exec" "create_json_script" {
+#    command = "scripts/create_json_script.sh"
+#}
 
   # Copy the three lines arc install script to target instance and run it from there
   provisioner "remote-exec" {
