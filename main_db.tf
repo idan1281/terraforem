f02e427fdae21afe3e47eb32faa089d25d49cc09
idan mishor <idan.mishor@sap.com>
2017-03-15 17:29:37 +0200

    changing the db_automation script name

--- main_db.tf
+++ main_db.tf
@@ -2,13 +2,13 @@
 variable "region" { default = "eu-de-1" } #Other region `au-ap-1`
 variable "image" { default = "sles-12-sp1-amd64-vmware-build98" }
 variable "guest_os" { default = "linux" }
-variable "key_pair" { default = "Limor Gargir-Alpert" }
+variable "key_pair" { default = "Idan-Pub2" }
 variable "flavor" { default = "20" }
 variable "security_group" { default = "default" }
 # variable "web_ip" { default = "10.47.1.63" } # Provide the floating ip you have created already via dashboard. Comment this line if you are creating one using terraform.
 # variable "web_dns" { default = "web.consulting.c.eu-de-1.cloud.sap" } # You can pass this to script to automation scripts.
 variable "ssh_user_name" { default = "ccloud" } 
-variable "ssh_key_path" { default = "/Users/c5215768/.ssh/id_rsa" } # On macOS, it is `/Users/<your_user_name>/.ssh/id_rsa`; on Windows `C:\Users\<your_user_name>\.ssh\id_rsa`
+variable "ssh_key_path" { default = "/Users/c5240533/.ssh/id_rsa" } # On macOS, it is `/Users/<your_user_name>/.ssh/id_rsa`; on Windows `C:\Users\<your_user_name>\.ssh\id_rsa`
 
 # Provider section
 provider "openstack" {
--- scripts/automation_create_db.sh
+++ scripts/automation_create_db.sh
@@ -8,11 +8,9 @@ AUTOMATION_NAME=limor-s4h-db
 AUTOMATION_REPO=https://github.wdf.sap.corp/c5240533/hana.git
 REPO_REVISION=master
 RUNLIST="recipe[hana::install-s4h-db-cal]"
-<<<<<<< HEAD
 ATTRIB_FILE=scripts/attributes_db.json
-=======
-ATTRIB_FILE="/Users/c5240533/dev/terraform/s4h_terraform/scripts/attributes_db.json"
->>>>>>> 6305be1af11e67ffe2aeeb26c5687cb54fb9db91
+#ATTRIB_FILE="/Users/c5240533/dev/terraform/s4h_terraform/scripts/attributes_db.json"
+
 # authentication using lyra to get token
 lyra authenticate 2>&1 | tee tmp/token_export.sh
 
@@ -29,11 +27,7 @@ AUTOMATION_ID=`awk -v auto_name=$AUTOMATION_NAME '$4==auto_name {print $2}' tmp/
 if [[ -z "$AUTOMATION_ID" ]]; then
   lyra automation create chef --name=$AUTOMATION_NAME --repository=$AUTOMATION_REPO \
     --runlist=$RUNLIST  --timeout=3000 \
-<<<<<<< HEAD
-    --attributes-from-file=$ATTRIB_FILE --repository-revision=$REPO_REVISION --log-level=debug 2>&1 | tee tmp/automation_created.txt
-=======
     --attributes-from-file=$ATTRIB_FILE --repository-revision=$REPO_REVISION --log-level=debug  2>&1 | tee tmp/automation_created.txt
->>>>>>> 6305be1af11e67ffe2aeeb26c5687cb54fb9db91
   AUTOMATION_ID=`awk '$2=="id" {print $4}' tmp/automation_created.txt`
 fi
 
@@ -41,11 +35,8 @@ fi
 #Add tag to server
 lyra node tag add --node-id $INSTANCE_ID name:s4h-db
 # Execute automation
-<<<<<<< HEAD
 lyra automation execute --automation-id $AUTOMATION_ID --selector='@identity="'$INSTANCE_ID'"' --watch 2>&1 | tee tmp/run_automation.txt
-=======
-lyra automation execute --automation-id $AUTOMATION_ID --selector '@identity="'$INSTANCE_ID'"' --watch 2>&1 | tee tmp/run_automation.txt
 
->>>>>>> 6305be1af11e67ffe2aeeb26c5687cb54fb9db91
+
 # Cleanup
 #rm -f tmp/*.txt tmp/token_export.sh
