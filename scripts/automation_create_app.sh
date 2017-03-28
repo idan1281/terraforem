e530a68ca1c8ff6068b2833690fd5d78ccc88f08
idan mishor <idan.mishor@sap.com>
2017-03-28 12:25:55 +0300

    uploading latest changes

--- scripts/automation_create_app.sh
+++ scripts/automation_create_app.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 # Make sure you source the openrc file before executing this script locally
+export INSTANCE_ID=$1
 
 read SRV_NAME < srv_name
 AUTOMATION_NAME=s4h-app
 AUTOMATION_REPO=https://github.wdf.sap.corp/c5215768/saperp.git
 REPO_REVISION=master
-RUNLIST="recipe[saperp::install-s4h-app-cal]"
+#RUNLIST="recipe[saperp::install-s4h-app-cal]recipe[sap-lvm::application]"
+RUNLIST="recipe[sap-lvm::application],recipe[saperp::install-s4h-app-cal]"
+ATTRIB_FILE=scripts/attributes_app.json
+
+
 #ATTRIB_FILE="/Users/c5240533/dev/terraform/s4h_terraform/scripts/attributes.json"
 # authentication using lyra to get token
 lyra authenticate 2>&1 | tee tmp/token_export.sh
@@ -27,8 +32,11 @@ if [[ -z "$AUTOMATION_ID" ]]; then
   AUTOMATION_ID=`awk '$2=="id" {print $4}' tmp/automation_created.txt`
 fi
 
+#Add tag to server
+lyra node tag add --node-id $INSTANCE_ID name:s4h-app
+
 # Execute automation
-lyra automation execute --automation-id $AUTOMATION_ID --selector '@hostname="idan-s4h-app"' --watch 2>&1 | tee tmp/run_automation.txt
+lyra automation execute --automation-id $AUTOMATION_ID --selector --selector='@identity="'$INSTANCE_ID'"' --watch 2>&1 | tee tmp/run_automation.txt
 
 # Cleanup
 rm -f tmp/*.txt tmp/token_export.sh
