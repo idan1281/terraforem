fef115b210f62fec670d113c0aead896f7994ee8
idan mishor <idan.mishor@sap.com>
2017-03-29 09:01:02 +0300

    comment out all variables in main files

--- main_app.tf
+++ main_app.tf
@@ -1,14 +1,14 @@
 # Variables section
-variable "region" { default = "eu-de-1" } #Other region `au-ap-1`
-variable "image" { default = "sles-12-sp1-amd64-vmware-build85" }
-variable "guest_os" { default = "linux" }
-variable "key_pair" { default = "Idan-Pub2" }
-variable "flavor" { default = "20" }
-variable "security_group" { default = "default" }
+#variable "region" { default = "eu-de-1" } #Other region `au-ap-1`
+#variable "image" { default = "sles-12-sp1-amd64-vmware-build85" }
+#variable "guest_os" { default = "linux" }
+#variable "key_pair" { default = "Idan-Pub2" }
+#variable "flavor" { default = "20" }
+#variable "security_group" { default = "default" }
 # variable "web_ip" { default = "10.47.1.63" } # Provide the floating ip you have created already via dashboard. Comment this line if you are creating one using terraform.
 # variable "web_dns" { default = "web.consulting.c.eu-de-1.cloud.sap" } # You can pass this to script to automation scripts.
-variable "ssh_user_name" { default = "suse" } # This varies; for redhat - fedora; for sles - root
-variable "ssh_key_path" { default = "/Users/c5240533/.ssh/id_rsa" } # On macOS, it is `/Users/<your_user_name>/.ssh/id_rsa`; on Windows `C:\Users\<your_user_name>\.ssh\id_rsa`
+#variable "ssh_user_name" { default = "suse" } # This varies; for redhat - fedora; for sles - root
+#variable "ssh_key_path" { default = "/Users/c5240533/.ssh/id_rsa" } # On macOS, it is `/Users/<your_user_name>/.ssh/id_rsa`; on Windows `C:\Users\<your_user_name>\.ssh\id_rsa`
 
 # Provider section
 provider "openstack" {
